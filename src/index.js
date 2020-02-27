a358a99afb4da79a4b2906aababc331fae15b4d8
juliebulakh <bulakh.julie@gmail.com>
2020-02-27 23:25:15 +0300

    init: add task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+  return (arguments.length&&array.length)?array.sort()[0]:0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return (arguments.length&&array.length)?array.sort().reverse()[0]:0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return (arguments.length&&array.length)?array.reduce((a,b)=>a+b)/array.length:0;
 }
