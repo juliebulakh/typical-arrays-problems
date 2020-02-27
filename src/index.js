e2ae41364136fc4675b1ad0567805ba052a0d74d
juliebulakh <bulakh.julie@gmail.com>
2020-02-27 23:39:52 +0300

    fix: fix solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return (arguments.length&&array.length)?array.sort()[0]:0;
+  return arguments.length&&array.length?array.sort((a,b)=>a-b)[0]:0;
 }
 
 exports.max = function max (array) {
-  return (arguments.length&&array.length)?array.sort().reverse()[0]:0;
+  return arguments.length&&array.length?array.sort((a,b)=>a-b).reverse()[0]:0;
 }
 
 exports.avg = function avg (array) {
-  return (arguments.length&&array.length)?array.reduce((a,b)=>a+b)/array.length:0;
+  return arguments.length&&array.length?array.reduce((a,b)=>a+b)/array.length:0;
 }
